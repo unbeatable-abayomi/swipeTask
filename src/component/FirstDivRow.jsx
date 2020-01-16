0eaaea3736351923590aa8f357405f2cfda02754
abayomi igwubor <igwubor@outlook.com>
2020-01-16 03:49:37 +0100

    completedCircleAddition

--- src/component/FirstDivRow.jsx
+++ src/component/FirstDivRow.jsx
@@ -2,45 +2,23 @@ import React from 'react';
 
 function FirstDivRow() {
 	return (
-        <div>
-        {/*
-        <div className="firstdivrow">
-                <div className="div_one_first" >
-                <center>
-                <div className='inside_content_cricle_one'></div>
-                </center> 
-                </div>
-                <div className="div_two_first" ><center>
-                <div className='inside_content_cricle'></div>
-                </center>  </div>
-				<div className="div_three_first" />
-				<div className="div_four_first" />
-				<div className="div_five_first" />
-				
-				
-				
+		<div>
+			<div class="scroll_divs">
+            <div id='div_circle'></div>    
+            <div />
+				<div />
+				<div />
+				<div />
+				<div />
+				<div />
+				<div />
+				<div />
+				<div />
+				<div />
+                <div />
+               
             </div>
-        */}
-			
-            <div class="scroll_divs">
-  <div></div>
-  <div></div>
-  <div>
-  
-  
-  
-   
-  </div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
- 
-</div>
+            
 		</div>
 	);
 }
